177a1da5a663271e0c1ec83569c61d3949a7ddd9
Vadim Dubrovskiy <programmer@Mac-mini-Programmer.local>
2019-10-22 16:21:58 +0300

    Add kivymd to project

---
+++ main.py
@@ -0,0 +1,124 @@
+from views.chat.chat import ChatWindow,RV
+from kivy.app import App
+from kivy.lang import Builder
+
+from kivymd.uix.navigationdrawer import NavigationDrawerIconButton
+from kivymd.theming import ThemeManager
+from kivymd.toast import toast
+from views.base.base import ContentNavigationDrawer
+
+main_kv = """
+<ContentNavigationDrawer@MDNavigationDrawer>:
+    drawer_logo: 'demos/kitchen_sink/assets/drawer_logo.png'
+
+    NavigationDrawerSubheader:
+        text: "Menu:"
+
+
+NavigationLayout:
+    id: nav_layout
+
+    ContentNavigationDrawer:
+        id: nav_drawer
+
+    BoxLayout:
+        orientation: 'vertical'
+
+        MDToolbar:
+            id: toolbar
+            title: 'KivyMD Kitchen Sink'
+            md_bg_color: app.theme_cls.primary_color
+            background_palette: 'Primary'
+            background_hue: '500'
+            elevation: 10
+            left_action_items:
+                [['dots-vertical', lambda x: app.root.toggle_nav_drawer()]]
+
+        Widget:
+"""
+
+class ChatApp(App):
+    theme_cls = ThemeManager()
+    # theme_cls.primary_palette = 'Teal'
+
+    def build(self):
+        # self.main_widget = Builder.load_string(main_kv)
+        # return self.main_widget
+        return ChatWindow()
+    #
+    # def callback(self, instance, value):
+    #     toast("Pressed item menu %d" % value)
+    #
+    # def on_start(self):
+    #     for i in range(15):
+    #         self.main_widget.ids.nav_drawer.add_widget(
+    #             NavigationDrawerIconButton(
+    #                 icon='checkbox-blank-circle', text="Item menu %d" % i,
+    #                 on_release=lambda x, y=i: self.callback(x, y)))
+
+
+if __name__ == '__main__':
+    ChatApp().run()
+
+
+
+# from kivy.app import App
+# from kivy.lang import Builder
+# from kivy.uix.boxlayout import BoxLayout
+#
+# from kivymd.icon_definitions import md_icons
+# from kivymd.uix.tab import MDTabsBase
+# from kivymd.theming import ThemeManager
+#
+# demo = """
+# <Example@BoxLayout>
+#     orientation: 'vertical'
+#
+#     MDToolbar:
+#         title: app.title
+#         md_bg_color: app.theme_cls.primary_color
+#         background_palette: 'Primary'
+#         left_action_items: [['menu', lambda x: x]]
+#
+#     MDTabs:
+#         id: android_tabs
+#
+#
+# <MyTab>
+#
+#     FloatLayout:
+#
+#         MDLabel:
+#             text: 'Content'
+#             halign: 'center'
+#             theme_text_color: 'Primary'
+#             font_style: 'H6'
+#
+#
+# """
+#
+# if __name__ == '__main__':
+#     from kivy.factory import Factory
+#     from kivymd.uix.button import MDIconButton
+#
+#
+#     class MyTab(BoxLayout, MDTabsBase):
+#         pass
+#
+#
+#     class Example(App):
+#         title = 'Example Tabs'
+#         theme_cls = ThemeManager()
+#         list_name_icons = list(md_icons.keys())[0:15]
+#
+#         def build(self):
+#             Builder.load_string(demo)
+#             screen = Factory.Example()
+#
+#             for name_tab in self.list_name_icons:
+#                 tab = MyTab(text=name_tab)
+#                 screen.ids.android_tabs.add_widget(tab)
+#             return screen
+#
+#
+# Example().run()
---
+++ views/chat/chat.py
@@ -0,0 +1,82 @@
+import os
+from kivy.lang import Builder
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.gridlayout import GridLayout
+from kivy.graphics import Canvas
+from kivy.factory import Factory
+from kivy.uix.label import Label
+
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.behaviors import FocusBehavior
+
+from kivy.properties import BooleanProperty
+from kivy.properties import NumericProperty
+from kivy.properties import StringProperty
+
+from kivymd.icon_definitions import md_icons
+from kivymd.uix.textfield import MDTextFieldRound
+from kivymd.uix.toolbar import MDToolbar
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.theming import ThemeManager
+
+Builder.load_file('views/chat/chat.kv')
+# Builder.load_file(os.path.join(os.path.dirname(__file__)), 'chat.kv')
+
+
+class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
+                                 RecycleBoxLayout):
+    pass
+
+
+class SelectableGrid(RecycleDataViewBehavior, Label):
+    ''' Add selection support to the Label '''
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+    cols = 1
+
+    def refresh_view_attrs(self, rv, index, data):
+        ''' Catch and handle the view changes '''
+        self.index = index
+        return super(SelectableGrid, self).refresh_view_attrs(
+            rv, index, data)
+
+    def on_touch_down(self, touch):
+        ''' Add selection on touch down '''
+        if super(SelectableGrid, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        ''' Respond to the selection of items in the view. '''
+        self.selected = is_selected
+        if is_selected:
+            print("selection changed to {0}".format(rv.data[index]))
+        else:
+            print("selection removed for {0}".format(rv.data[index]))
+
+
+class RV(RecycleView):
+    def __init__(self, **kwargs):
+        super(RV, self).__init__(**kwargs)
+        self.data = [{'text': str(x)} for x in range(10)]
+
+        # can also be performed in a complicated one liner for those who like it tricky
+        # self.data = [{'label2': {'text': i1}, 'label3': {'text': i2}} for i1, i2 in zip(items_1, items_2)]
+
+
+class ChatWindow(Widget):
+    def __init__(self, **kwargs):
+        super(ChatWindow, self).__init__(**kwargs)
+        # self.height -= 100
+        # self.width -= 100
+
+
+
+class Chat(Widget):
+    theme_cls = ThemeManager()
