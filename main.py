d08e84b74c48ebfed40c3a412400dc8f38e9e6bc
Vadim Dubrovskiy <programmer@Mac-mini-Programmer.local>
2019-10-25 16:50:23 +0300

    Add navigation panel on program

--- main.py
+++ main.py
@@ -1,54 +1,73 @@
 from views.chat.chat import ChatWindow,RV
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
-
-from kivymd.uix.navigationdrawer import NavigationDrawerIconButton
+from kivy.uix.widget import Widget
+from kivymd.uix.navigationdrawer import NavigationDrawerIconButton,NavigationDrawerSubheader, MDNavigationDrawer
 from kivymd.theming import ThemeManager
 from kivymd.toast import toast
 
 
 main_kv = """
+<ContentNavigationDrawer@MDNavigationDrawer>:
+    drawer_logo: 'demos/kitchen_sink/assets/drawer_logo.png'
 
- ChatWindow
-# <ContentNavigationDrawer@MDNavigationDrawer>:
-#     drawer_logo: 'demos/kitchen_sink/assets/drawer_logo.png'
-# 
-#     NavigationDrawerSubheader:
-#         text: "Menu:"
-# 
-# 
-# NavigationLayout:
-#     id: nav_layout
-# 
-#     ContentNavigationDrawer:
-#         id: nav_drawer
-# 
-#     BoxLayout:
-#         orientation: 'vertical'
-#        
-#         MDToolbar:
-#             id: toolbar
-#             title: 'KivyMD Kitchen Sink'
-#             md_bg_color: app.theme_cls.primary_color
-#             background_palette: 'Primary'
-#             background_hue: '500'
-#             elevation: 10
-#             left_action_items:
-#                 [['dots-vertical', lambda x: app.root.toggle_nav_drawer()]]
-# 
-#         Widget:
-         
-        
+    NavigationDrawerSubheader:
+        text: "Menu:"
+
+
+NavigationLayout:
+    id: nav_layout
+
+    ContentNavigationDrawer:
+        id: nav_drawer
+
+    BoxLayout:
+        orientation: 'vertical'
+
+        MDToolbar:
+            id: toolbar
+            title: 'KivyMD Kitchen Sink'
+            md_bg_color: app.theme_cls.primary_color
+            background_palette: 'Primary'
+            background_hue: '500'
+            elevation: 10
+            left_action_items:
+                [['dots-vertical', lambda x: app.root.toggle_nav_drawer()]]
+
+        Widget:
+            id:widget_data
 """
+
+class BaseNavigation(Widget):
+
+    def open(self):
+        self.ids['navdrawer'].toggle_nav_drawer()
+
 
 class ChatApp(App):
     theme_cls = ThemeManager()
     theme_cls.primary_palette = 'Teal'
-
     def build(self):
-
-        return ChatWindow()
-
+        return BaseNavigation()
+    # theme_cls = ThemeManager()
+    # theme_cls.primary_palette = 'Teal'
+    #
+    # def build(self):
+    #     self.main_widget = Builder.load_string(main_kv)
+    #     self.main_widget.ids['widget_data'] = ChatWindow()
+    #     return self.main_widget
+    #     # return ChatWindow()
+    #
+    # def callback(self, instance, value):
+    #     toast("Pressed item menu %d" % value)
+    #
+    # def on_start(self):
+    #     for i in range(15):
+    #         self.main_widget.ids.nav_drawer.add_widget(
+    #             NavigationDrawerIconButton(
+    #                 icon='checkbox-blank-circle', text="Item menu %d" % i,
+    #                 on_release=lambda x, y=i: self.callback(x, y)))
 
     # def callback(self, instance, value):
     #     toast("Pressed item menu %d" % value)
